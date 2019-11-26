e2829c6700aa0cc58ee4b351cd21c249b85385fc
Oskar <32549612+OskarPrzybylski@users.noreply.github.com>
2019-11-26 18:14:41 +0100

    Added endpoint for getting reversed string

---
+++ Controllers/StringController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace zadanie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringController : ControllerBase
+    {
+        public StringController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult ReverseString([FromQuery] string s)
+        {
+            char[] charArray = s.ToCharArray();
+			Array.Reverse( charArray );
+			return Ok(new string(charArray));
+        }
+    }
+}
