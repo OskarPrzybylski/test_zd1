37cd2bc22efc9e260a5875c82613277330e53cf4
Oskar <32549612+OskarPrzybylski@users.noreply.github.com>
2020-01-17 13:56:51 +0100

    Added models

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadanie.Models
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public string Title { get; set; }
+    }
+}
